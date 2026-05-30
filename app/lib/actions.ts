c7b20a9935a525dc28a5a99fb255da9b67d6af31
Sihan Chen <sihanchen@Sihans-MacBook-Pro.local>
2026-05-31 02:43:36 +0800

    update0531

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,10 +32,18 @@ export async function createInvoice(formData: FormData) {
   console.log(typeof amountInCents);
   console.log(amountInCents)
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    // return {
+    //   message: 'Database Error: Failed to Create Invoice.',
+    // };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -56,17 +64,25 @@ export async function updateInvoice(id: string, formData: FormData) {
   console.log(typeof amountInCents);
   console.log(amountInCents)
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    //return { message: 'Database Error: Failed to Update Invoice.' };
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  //throw new Error('Failed to Delete Invoice');
+
   await sql`
     DELETE FROM invoices
     WHERE id = ${id}
